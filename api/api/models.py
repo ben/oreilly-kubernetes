074b7e82c5bee6bc1c82cb1d87b48a5064b1df1c
Ben Straub <ben@straub.cc>
2015-12-20 11:46:19 -0800

    `id` is in the base class

--- api/api/models.py
+++ api/api/models.py
@@ -3,6 +3,5 @@ from .db import Base
 
 class Todo(Base):
     __tablename__ = 'todo'
-    id = sa.Column(sa.Integer, primary_key=True)
     title = sa.Column(sa.Unicode)
     state = sa.Column(sa.Unicode)
