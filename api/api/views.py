c61a225bf06d876e9d46e1f8383d107b995ead19
Ben Straub <ben@straub.cc>
2016-01-17 18:50:07 -0800

    Adjust headers and JSON format for Ember-Data

--- api/api/views.py
+++ api/api/views.py
@@ -3,7 +3,16 @@ from flask import jsonify, request
 
 from api import models, db
 
+def cors_headers(f):
+    def wrapped(*args, **kwargs):
+        resp = f(*args, **kwargs)
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
+    return wrapped
+
 class TodoApi(MethodView):
+    decorators = [cors_headers]
+
     def get(self, todo_id):
         if todo_id is None:
             todos = models.Todo.all()
@@ -12,13 +21,13 @@ class TodoApi(MethodView):
                 'title': t.title,
                 'state': t.state,
             } for t in todos]
-            return jsonify(items=objs)
+            return jsonify(todos=objs)
         t = models.Todo.query.get(todo_id)
-        return jsonify(
-            id=t.id,
-            title=t.title,
-            state=t.state
-        )
+        return jsonify(todo={
+            'id' : t.id,
+            'title' : t.title,
+            'state' : t.state,
+        })
 
     def post(self):
         t = models.Todo()
