e58e20e21b6046d658dc0a6af3a91e57bc587e97
Ben Straub <ben@straub.cc>
2016-06-25 12:37:45 -0700

    Send messages to mailgun

--- mailer/app/tasks/send_email.rb
+++ mailer/app/tasks/send_email.rb
@@ -1,8 +1,16 @@
+MAILGUN_KEY = ENV['MAILGUN_KEY']
+MAILGUN_DOMAIN = ENV['MAILGUN_DOMAIN']
+
 module SendEmail
   @queue = :mailer
 
   def self.perform(oe_id)
     oe = OutgoingEmail.find_by_id oe_id
     puts "Processing email to #{oe.to}"
+    client = Mailgun::Client.new MAILGUN_KEY
+    message_params = oe.as_json.slice('from', 'to', 'subject', 'text')
+    puts message_params
+    result = client.send_message MAILGUN_DOMAIN, message_params
+    puts result.body
   end
 end
