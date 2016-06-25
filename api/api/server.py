b0b3f712373603b27223e61da040240ba9e39a0d
Ben Straub <ben@straub.cc>
2016-06-25 16:43:09 -0700

    send_email endpoint for api

--- api/api/server.py
+++ api/api/server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, g
+from flask import Flask, g, request
 app = Flask(__name__)
 app.debug = True
 
@@ -8,6 +8,7 @@ from flask_cors import CORS
 CORS(app)
 
 from api import views, db, models
+import requests
 
 @app.teardown_request
 def teardown_request(exception):
@@ -19,6 +20,25 @@ def teardown_request(exception):
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    # print(request.json)
+    todos = models.Todo.all()
+    email_lines = ['TODOS:', '']
+    for t in todos:
+        # print(t.id, t.state)
+        email_lines.append(
+            '  ' + ('x' if t.state == 'complete' else ' ') + '  ' + t.title
+        )
+
+    requests.post('http://mailer:3000/outgoing_emails', data={
+        'to': request.json['to'],
+        'from': 'todos@example.com',
+        'subject': 'TODOs',
+        'text': '\n'.join(email_lines)
+    })
+    return '\n'.join(email_lines)
 
 todo_view = views.TodoApi.as_view('todo_api')
 app.add_url_rule('/todos', defaults={'todo_id': None},
